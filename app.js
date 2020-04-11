e0363ce2c10f3a52bde801a83dd729fd3e4d7801
evicoach@gmail.com <evicoach@gmail.com>
2020-04-11 09:06:27 +0100

    Created different route for the xml and json response

--- app.js
+++ app.js
@@ -9,10 +9,55 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/v1/on-covid-19', (req, res)=>{
-  const data = req.body.data;
-  const url = req.url;
-  const xmlOrJSON = url.substring(url.lastIndexOf('/'));
+
+app.get('/', (req, res) => {
+  res.send('You have to send a post request with input data!');
+});
+
+app.post('/api/v1/on-covid-19', (req, res, next) => {
+  next();
+});
+
+app.post('/api/v1/on-covid-19/json', (req, res)=>{
+  const data = JSON.parse(req.body.data);
+  res.setHeader("Content-Type", "application/json");
+  res.json(JSON.stringify(estimator(data)));
+
+  // const url = req.url;
+  // const xmlOrJSON = url.substring(url.lastIndexOf('/'));
+  /**
+   * if url ends with /json
+   *      set content type to json
+   *      respond with json formatted data
+   * if url ends with /xml
+   *      set content type to xml
+   *      respond with xml formatted data
+   */
+
+  //  if(xmlOrJSON === '/json'){
+  //   res.setHeader("Content-Type", "application/json");
+  //   res.json(estimator(data))
+  //  } else if (xmlOrJSON === '/xml'){
+  //   res.setHeader("Content-Type", "application/xml");
+  //   res.send(convert.js2xml(estimator(data)));
+  //  }
+});
+
+app.get('/api/v1/on-covid-19/logs', (req, res) => {
+  /**
+   * use morgan
+   * or
+   * create an object for each request that has the 
+   * {method, path, resultStatus code, time}
+   * push each object into an array
+   * persist the array
+   */
+});
+
+app.post('/api/v1/on-covid-19/json/xml', (req, res)=>{
+  const data = JSON.parse(req.body.data);
+  res.setHeader("Content-Type", "application/xml");
+  res.send(convert.js2xml(estimator(data)));
   /**
    * if url ends with /json
    *      set content type to json
