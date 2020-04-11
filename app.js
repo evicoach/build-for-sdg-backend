d83ca6dfd10dfba2f7a9acab09485469d39e87c8
evicoach@gmail.com <evicoach@gmail.com>
2020-04-11 11:17:07 +0100

    Made the necessary changes

--- app.js
+++ app.js
@@ -8,24 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-
-app.get('/', (req, res) => {
-  res.send('You have to send a post request with input data!');
-});
-
-app.post('/api/v1/on-covid-19', (req, res, next) => {
-  next();
-});
-
-app.post('/api/v1/on-covid-19/json', (req, res)=>{
-  const data = JSON.parse(req.body.data);
-  res.setHeader("Content-Type", "application/json");
-  res.json(JSON.stringify(estimator(data)));
-
-  // const url = req.url;
-  // const xmlOrJSON = url.substring(url.lastIndexOf('/'));
-  /**
+ /**
    * if url ends with /json
    *      set content type to json
    *      respond with json formatted data
@@ -33,14 +16,52 @@ app.post('/api/v1/on-covid-19/json', (req, res)=>{
    *      set content type to xml
    *      respond with xml formatted data
    */
+const getDurationInMilliseconds = (start)=>{
+  const NS_PER_SEC = 1e9;
+  const NS_TO_MS = 1e6;
+  const diff = process.hrtime(start);
 
-  //  if(xmlOrJSON === '/json'){
-  //   res.setHeader("Content-Type", "application/json");
-  //   res.json(estimator(data))
-  //  } else if (xmlOrJSON === '/xml'){
-  //   res.setHeader("Content-Type", "application/xml");
-  //   res.send(convert.js2xml(estimator(data)));
-  //  }
+  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
+}
+
+const jsonRes = (req, res)=>{
+  const data = req.body;
+  res.setHeader("Content-Type", "application/json");
+  res.status(200);
+  res.send(JSON.stringify(estimator(data)));
+}
+app.use((req, res, next)=>{
+  const start = process.hrtime();
+  res.on('finish', ()=>{
+    const durationInMilliseconds =  parseInt(getDurationInMilliseconds(start));
+    console.log(`${req.method} ${req.path} ${res.statusCode} ${durationInMilliseconds}\n ms`);
+  });
+  res.on('close', ()=>{
+    const durationInMilliseconds =  parseInt(getDurationInMilliseconds(start));
+    console.log(`${req.method} ${req.path} ${res.statusCode} ${durationInMilliseconds}\n ms`);
+  });
+
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('You have to send a post request with input data!');
+});
+
+app.post('/api/v1/on-covid-19', (req, res, next) => {
+    jsonRep(req, res);
+});
+
+app.post('/api/v1/on-covid-19/json', (req, res)=>{
+  jsonRes(req, res);
+});
+
+app.post('/api/v1/on-covid-19/xml', (req, res)=>{
+  const data = req.body;
+  const options = {compact: true, ignoreComment: true, spaces: 4};
+  res.setHeader("Content-Type", "application/xml");
+  res.status(200);
+  res.send(convert.json2xml(JSON.stringify(estimator(data)), options));
 });
 
 app.get('/api/v1/on-covid-19/logs', (req, res) => {
@@ -52,28 +73,6 @@ app.get('/api/v1/on-covid-19/logs', (req, res) => {
    * push each object into an array
    * persist the array
    */
-});
-
-app.post('/api/v1/on-covid-19/json/xml', (req, res)=>{
-  const data = JSON.parse(req.body.data);
-  res.setHeader("Content-Type", "application/xml");
-  res.send(convert.js2xml(estimator(data)));
-  /**
-   * if url ends with /json
-   *      set content type to json
-   *      respond with json formatted data
-   * if url ends with /xml
-   *      set content type to xml
-   *      respond with xml formatted data
-   */
-
-   if(xmlOrJSON === '/json'){
-    res.setHeader("Content-Type", "application/json");
-    res.json(estimator(data))
-   } else if (xmlOrJSON === '/xml'){
-    res.setHeader("Content-Type", "application/xml");
-    res.send(convert.js2xml(estimator(data)));
-   }
 });
 
 app.listen(port, ()=>{
