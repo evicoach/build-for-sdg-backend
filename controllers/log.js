87c6f5ef509d9d80e33635bc00a4b0611ba513ea
evicoach@gmail.com <evicoach@gmail.com>
2020-04-13 00:55:38 +0100

    set the MIME type to text/plain

--- controllers/log.js
+++ controllers/log.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const log = (req, res)=>{
   const logFile = path.resolve(path.join(__dirname,'../log.txt'));
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200);
   res.sendFile(logFile);
 }
 
