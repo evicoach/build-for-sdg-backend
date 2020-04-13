e5b8209b96a0cf87ad83fbd0fc56ddece139e73c
evicoach@gmail.com <evicoach@gmail.com>
2020-04-13 01:23:30 +0100

    Made some changes

--- controllers/log.js
+++ controllers/log.js
@@ -3,6 +3,7 @@ const path = require('path');
 const log = (req, res)=>{
   const logFile = path.resolve(path.join(__dirname,'../log.txt'));
   res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Location", "https://covid-19-estimator-sdg.herokuapp.com/api/v1/on-covid-19/logs");
   res.status(200);
   res.sendFile(logFile);
 }
