263690d20c241e2ae10798a96f3f1006db0af880
evicoach@gmail.com <evicoach@gmail.com>
2020-04-13 16:04:39 +0100

    Made some more changes

--- controllers/logger.js
+++ controllers/logger.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const getDurationInMilliseconds = require('./durationInMilliseconds');
 
 const logger = (req, res, startTime)=>{
-  const durationInMilliseconds =  getDurationInMilliseconds(startTime);
+  let durationInMilliseconds =  getDurationInMilliseconds(startTime);
+  if(durationInMilliseconds < 10) durationInMilliseconds = `0${durationInMilliseconds}`;
     console.log(`${req.method}\t\t${req.path}\t\t${res.statusCode}\t\t${durationInMilliseconds}ms\n`);
     let statusCode = res.statusCode;
     if(req.path === '/api/v1/on-covid-19/logs'){
       statusCode = 200;
     } 
+
     const log = `${req.method}\t\t${req.path}\t\t${statusCode}\t\t${durationInMilliseconds}ms\n`;
 
     fs.writeFileSync('log.txt', log, {flag: 'a', encoding: 'utf8'});
